2cbeab6e3572a77362a6e11fda8d8a9be96bbfa0
Adan Nugraha <contact.ardana@gmail.com>
2021-11-24 09:09:40 +0700

    setup API

--- helpers.py
+++ helpers.py
@@ -1,17 +1,17 @@
 import os
 import requests
 import urllib.parse
-
+ 
 from flask import redirect, render_template, request, session
 from functools import wraps
-
-
+ 
+ 
 def apology(message, code=400):
     """Render message as an apology to user."""
     def escape(s):
         """
         Escape special characters.
-
+ 
         https://github.com/jacebrowning/memegen#special-characters
         """
         for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
@@ -19,12 +19,12 @@ def apology(message, code=400):
             s = s.replace(old, new)
         return s
     return render_template("apology.html", top=code, bottom=escape(message)), code
-
-
+ 
+ 
 def login_required(f):
     """
     Decorate routes to require login.
-
+ 
     https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
     """
     @wraps(f)
@@ -33,11 +33,11 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
-
-
+ 
+ 
 def lookup(symbol):
     """Look up quote for symbol."""
-
+ 
     # Contact API
     try:
         api_key = os.environ.get("API_KEY")
@@ -46,7 +46,7 @@ def lookup(symbol):
         response.raise_for_status()
     except requests.RequestException:
         return None
-
+ 
     # Parse response
     try:
         quote = response.json()
@@ -57,8 +57,8 @@ def lookup(symbol):
         }
     except (KeyError, TypeError, ValueError):
         return None
-
-
+ 
+ 
 def usd(value):
     """Format value as USD."""
     return f"${value:,.2f}"
